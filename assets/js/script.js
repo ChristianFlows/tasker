46a1e487f70a601d4f8b6b4263dc759d761639b5
Christian Anderson <40026509+ChristianFlows@users.noreply.github.com>
2021-03-09 21:33:11 -0500

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,25 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 var createTaskHandler = function(event) { 
-    event.preventDefault(); 
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // list item creation
     var listItemEl = document.createElement("li"); 
     listItemEl.className = "task-item"; 
-    listItemEl.textContent = "This is a new task."; 
-    tasksToDoEl.appendChild(listItemEl); 
-    }; 
 
-    formEl.addEventListener("submit", createTaskHandler);
+    // div to contain task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl); 
+}; 
+
+formEl.addEventListener("submit", createTaskHandler);
